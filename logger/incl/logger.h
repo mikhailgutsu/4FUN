4924289668ff52e065172c062d524258b62969e2
Frenki <123064601+mikhailgutsu@users.noreply.github.com>
2024-07-30 12:11:30 +0300

    Update logger.h

--- logger/incl/logger.h
+++ logger/incl/logger.h
@@ -1,9 +1,17 @@
-#include <iostream>
-
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <ctime>
 
+class Logger {
+private:
 
+public:
+    void SetColor(int color);
+    void log(const std::string& message);
+};
 
 #endif // LOGGER_H
